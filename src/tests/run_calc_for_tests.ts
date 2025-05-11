7d0b3ee9801e3a90387f2cf18fd19d269108f7ec
Jared Hughes <jahughes241@gmail.com>
2025-05-10 22:37:47 -0700

    Run replacements in unit tests

--- src/tests/run_calc_for_tests.ts
+++ src/tests/run_calc_for_tests.ts
@@ -1,5 +1,11 @@
-/* eslint-disable import/first */
+
 /* eslint-disable no-console */
+import fs from "fs";
+import path from "path";
+
+import { replacements } from "../preload/moduleReplacements";
+import { fullReplacement } from "../../apply-replacements/applyReplacement";
+
 document.body.dataset.loadData = JSON.stringify({ seed: "c29cb3444b1c435c8e4422a19ebf56f5" });
 document.body.innerHTML = `
   <div class="dcg-sliding-interior">
@@ -36,4 +42,29 @@ HTMLMediaElement.prototype.play = async () => {
 }
 console.debug = () => {}
 
-import "../../node_modules/.cache/desmos/calculator_desktop.js";
+let codeCache: string | undefined;
+function getCalcDesktopCode() {
+  if (codeCache) return codeCache;
+
+  const file = path.join(__dirname, "../../node_modules/.cache/desmos/calculator_desktop.js");
+  const calculatorDesktopRaw = fs.readFileSync(file, { encoding: "utf-8" });
+
+  const repl = fullReplacement(calculatorDesktopRaw, replacements);
+  codeCache = repl.newCode
+  
+  describe("Replacements are good", () => {
+    test("No replacement errors", () => {
+      expect(repl.blockFailures).toStrictEqual([]);
+      expect(repl.otherErrors).toStrictEqual([]);
+    })
+  
+    test("Replacements makes the code longer", () => {
+      expect(repl.newCode.length).toBeGreaterThan(calculatorDesktopRaw.length + 1000)
+    })
+  })
+
+  return codeCache
+}
+
+// eslint-disable-next-line no-eval
+eval(getCalcDesktopCode())
